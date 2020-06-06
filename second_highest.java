e01909be24f3718f1136b71631655a61501e869c
hymhanraj <53473311+hymhanraj@users.noreply.github.com>
2020-06-06 23:06:12 +0530

    Update second_highest.java

--- second_highest.java
+++ second_highest.java
@@ -20,4 +20,5 @@ int a[]={1,2,5,6,3,2};
 int b[]={44,66,99,77,33,22,55};  
 System.out.println("Second Largest: "+getSecondLargest(a,6));  
 System.out.println("Second Largest: "+getSecondLargest(b,7));  
-}}
+}
+}
